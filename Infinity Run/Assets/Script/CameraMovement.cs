0326d57fde98fa86b0531ac3f0bb2ec7536799a6
Muhammed Mohab <m.cheetos_898@yahoo.com>
2019-04-12 01:10:44 +0200

    V2
    Added prefabs/randomzied function to the tiles

--- Infinity Run/Assets/Script/CameraMovement.cs
+++ Infinity Run/Assets/Script/CameraMovement.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    private Transform lookAt;
+    private Vector3 offset;
+    private Vector3 movement;
+    private Vector3 animOffset = new Vector3(0, 5, 5);
+    private float strtrans = 0.0f; // starting transation;
+    private float animtime = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 4);
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - lookAt.position; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement = lookAt.position + offset;
+        if(strtrans > 1.0f)
+        {
+            //Normal camera movement + offset;
+            transform.position = movement;
+        }
+        else
+        {
+            //Camera Start animation;
+            transform.position = Vector3.Lerp(movement + animOffset, movement, strtrans);
+            strtrans = strtrans + Time.deltaTime * 1 / animtime;
+
+        }
+
+
     }
 }
